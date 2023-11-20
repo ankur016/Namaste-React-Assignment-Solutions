6bab400457dafe2a0e5280068e77278c5cb06937
ankurdubey <dubeyankur016@gmail.com>
2023-11-20 20:40:00 +0530

    Assignment 2

---
+++ Assignment_2/app.js
@@ -0,0 +1,18 @@
+import React from "react";
+import  ReactDOM  from "react-dom/client";
+
+const heading1 = React.createElement("h1", {
+    id : "title1",
+}, "Heading1");
+
+const heading2 = React.createElement("h2", {
+    id : "title2",
+}, "Heading2");
+
+const myDiv =React.createElement("div", {
+    id: "container"
+}, [heading1, heading2]);
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
+root.render(myDiv);
